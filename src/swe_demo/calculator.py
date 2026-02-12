3f6b159c1a8a205e76cc1af3fdb7de760af22631
vsstech <sivaram2019@Outlook.com>
2026-02-11 22:41:24 -0600

    Add subtract and multiply functions to calculator

--- src/swe_demo/calculator.py
+++ src/swe_demo/calculator.py
@@ -9,3 +9,7 @@ def is_even(n: int) -> bool:
     
 def add(a: float, b: float) -> float:
     return a+b
+def substract(a: float, b: float) -> float:
+    return a-b
+def multiply(a: float, b: float) -> float:
+    return a*b
